ca0e4e33cfe7bb0c65d5c2b27abb1850cacfdead
Bennett Smrdel <smrdelb@gmail.com>
2021-11-12 19:42:20 -0600

    addition will work for posted numbers

--- server/public/client.js
+++ server/public/client.js
@@ -1,24 +1,50 @@
 $(document).ready(onReady);
 
+let plusButtonValue = 0;
+// let subButtonValue = 0;
+// let multiplyButtonValue = 0;
+// let divideButtonValue = 0;
+
 function onReady() {
+    $('#plus-btn').on('click', plusButton);
     // Post numbers
     $('#submit-btn').on('click', getNumbers);
 }
 
 function getNumbers() {
-    // Store values of input fields
-    let inputs = {
-        number1: $('#number-1').val(),
-        number2: $('#number-2').val()
+    // Check if fields are empty
+
+    // Check if any button is selected
+    if (plusButtonValue === 0 ) {
+        alert('Please select a math opperation!');
+    } else {
+        // Store values of input fields
+        let inputs = {
+            number1: $('#number-1').val(),
+            number2: $('#number-2').val(),
+            plusBtn: plusButtonValue,
+        }
+        // Use ajax POST request
+        // Sends object data to server to modifify it, and GET request it when needed.
+        $.ajax({
+            type: 'POST',
+            url: '/numbers',
+            data: inputs
+        }).then(function(response) {
+            console.log('POST worked!');
+        }).catch(function(error) {
+            console.log('error', error);
+        });
     }
-    // use ajax POST request
-    $.ajax({
-        type: 'POST',
-        url: '/numbers',
-        data: inputs
-    }).then(function(response) {
-        console.log('POST worked!');
-    }).catch(function(error) {
-        console.log('error', error);
-    });
 }
+
+function plusButton() {
+    // Check if field is empty
+
+    // plus 1 to button score
+    plusButtonValue = 1;
+    console.log('button value', plusButtonValue);
+    // Set all other button scores to 0
+    // !plusButtonValue = 0;
+}
+
--- server/server.js
+++ server/server.js
@@ -4,12 +4,20 @@ const PORT = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Use ansers.length - 1 in order to access the most recent answer.
 let answers = [];
 
-
+// Data is being sent to the server via POST, in order for the server side script to use it.
 app.post('/numbers', (req, res) => {
+    let number1 = Number(req.body.number1);
+    let number2 = Number(req.body.number2);
+
     console.log('numbers object:', req.body);
-    answers.push(answers);
+    if (req.body.plusBtn === '1') {
+        let sum = number1 + number2;
+        answers.push(sum);
+    }
+    console.log('answers:', answers);
     res.sendStatus(201);
 });
 
